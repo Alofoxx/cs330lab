6be4b7e2592e2f83e72fb8a25a8da7b52a5544d5
alofoxx <alofoxx@Civet.alofoxx.com>
2016-01-19 18:57:12 -0800

    Updated selection sort

--- lab2/selection_sort.cpp
+++ lab2/selection_sort.cpp
@@ -3,3 +3,46 @@
  * selection_sort.cpp
  * JAN 25 2016
  ******************************************************************************/
+
+
+#include <iostream>
+#include <functional>   // modulus,
+#include <stdlib.h>     /* srand, rand */
+#include <utility>      // swap
+#include <time.h>       /* time */
+#include <vector>
+
+using namespace std;
+
+
+
+
+main()
+{
+    int n;
+
+    cout << "Please enter a number" << endl;
+    cin >> n;
+
+    vector<int> v(n);
+
+    srand(time(0));
+
+    for (int k = 0; k < n; k++ )
+        v[k] = rand() % 1000000;
+
+    //selection sort
+    for (int i = 0; i < n-1; i++)
+        for (int j = i+1; j < n; j++)
+            if (v[i] > v[j])
+                swap(v[i], v[j]);
+
+
+    cout << endl << endl;
+    for (int i = 0; i < n; i++ )
+        cout << v[i] << endl; 
+
+}
+
+
+
