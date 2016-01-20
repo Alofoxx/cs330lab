00748c7a9a17399501c1d86bfcd1d59d4e6dcace
alofoxx <alofoxx@Civet.alofoxx.com>
2016-01-20 09:07:57 -0800

    finished up selection and insertion sorts

--- lab2/insertion_sort.cpp
+++ lab2/insertion_sort.cpp
@@ -2,22 +2,17 @@
  * Patrick Gillespie
  * insertion_sort.cpp
  * JAN 25 2016
+ * This program creates a vector <input> rand numbers between 0 - 1000000 and
+ * then sorts the vector using insertion sort. Numbers are then output in order.
  ******************************************************************************/
-
-
-
-
 #include <iostream>
-#include <functional>   // modulus,
+#include <functional>   /* modulus */
 #include <stdlib.h>     /* srand, rand */
-#include <utility>      // swap
+#include <utility>      /* swap */
 #include <time.h>       /* time */
 #include <vector>
 
 using namespace std;
-
-
-
 
 main()
 {
@@ -33,10 +28,6 @@ main()
     for (int k = 0; k < n; k++ )
         v[k] = rand() % 1000000;
 
-
-
-
-
     int j, temp;
 		
     for (int i = 0; i < n; i++){
@@ -50,20 +41,8 @@ main()
 	}
     }
 
-/*
-    //insertion sort
-    int j;
-    for (int i = 1; i < n; i++){
-        for (int elem = v[i], j = i-1; j >=0 and elem < v[j]; j--){
-            v[j+1] = v[j];
-        }    
-        v[j+1] = elem;
-    }
-*/
-
     cout << endl << endl;
     for (int i = 0; i < n; i++ )
         cout << v[i] << endl; 
 
-}
-
+} // main
--- lab2/selection_sort.cpp
+++ lab2/selection_sort.cpp
@@ -2,20 +2,17 @@
  * Patrick Gillespie
  * selection_sort.cpp
  * JAN 25 2016
+ * This program creates a vector <input> rand numbers between 0 - 1000000 and
+ * then sorts the vector using selection sort. Numbers are then output in order.
  ******************************************************************************/
-
-
 #include <iostream>
-#include <functional>   // modulus,
+#include <functional>   /* modulus */
 #include <stdlib.h>     /* srand, rand */
-#include <utility>      // swap
+#include <utility>      /* swap */
 #include <time.h>       /* time */
 #include <vector>
 
 using namespace std;
-
-
-
 
 main()
 {
@@ -37,12 +34,7 @@ main()
             if (v[i] > v[j])
                 swap(v[i], v[j]);
 
-
     cout << endl << endl;
     for (int i = 0; i < n; i++ )
         cout << v[i] << endl; 
-
-}
-
-
-
+} // main
