f1eedc7d1c6069149a7220caa19320fa1662da3c
alofoxx <alofoxx@Civet.alofoxx.com>
2016-02-07 14:27:21 -0800

    Updated string.h

--- lab3/PGString.h
+++ lab3/PGString.h
@@ -1,23 +1,25 @@
 /******************************************************************************
  * Patrick Gillespie
- * PGString.h
+ * String.h
  * FEB 07 2016
  * This header defines a simple string class.
  ******************************************************************************/
 
-#ifndef __PGLIB__PGString__H
-#define __PGLIB__PGString__H
+#ifndef __PGLIB__String__H
+#define __PGLIB__String__H
 
 // String.h
 
 #include <iostream>
+#include <cstddef> /* size_t, nullptr */
 
 using namespace std;
 
 // simple string class
 class String {
-        int size;
-        char * buffer;
+    char * buffer = nullptr;
+    size_t size = 0;
+
 public:
     // constructors
     String();
@@ -27,13 +29,32 @@ public:
     // deconstrutor
     ~String();
 
-    // main methods
+    // basic methods
     char * begin();
     char * end();
     int length();
-    char & operator[](const int);
-    string operator=(const String &);
-    string operator+=(const String &);
+
+    // operators
+    String operator =  ( const String & );
+    String operator += ( const String & );
+    String operator += ( const char * );
+    char & operator [] (const int);
+
+    // comparison operators
+    bool operator == ( const String & );
+    bool operator != ( const String & );
+    bool operator >  ( const String & );
+    bool operator <  ( const String & );
+    bool operator >= ( const String & );
+    bool operator <= ( const String & );
+
+    // utility methods
+    size_t length() const { return _str_len; }
+    size_t size() const { return _str_len; }
+    const char & begin();
+    const char & end();
+
+
 
     // other methods
     friend bool operator==(const String &, const String &);
@@ -42,4 +63,7 @@ public:
     friend ostream & operator<<(ostream &, const String &);
 } // String
 
-#endif
+// non-member operator overloads
+String operator + ( const String & lhs, const String & rhs );
+
+#endif // __PGLIB__String__H
