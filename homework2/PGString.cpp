78a6bddaa2edd09de61a68d826f31528820e4ad4
alofoxx <alofoxx@Civet.alofoxx.com>
2016-02-09 07:59:26 -0800

    updated

--- homework2/PGString.cpp
+++ homework2/PGString.cpp
@@ -226,14 +226,12 @@ main()
 {
    String s1 = "abc";
    String s2 = "def";
-   String s3 = s1;
-   s3 + s2; // should be abcdef
+   String s3 = s1 + s2; // should be abcdef
    cout << s1 << " + " << s2 << " = " s3 << endl;
 
    String s4 = "abcbbccdf";
    String s5 = "bc";
-   String s6 = s4;
-   s6 - s5; // should be abcdf
+   String s6 = s4 - s5; // should be abcdf
    cout << s4 << " - " << s5 << " = " s6 << endl;
    
    String s7 = s1 * 3; // should be abcabcabc
